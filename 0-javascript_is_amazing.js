d75246ae11b8bc3d8dbf4c1405532c2acf286e01
rt777-333 <tribka7@gmail.com>
2025-05-25 20:58:48 +0300

    Update 0-javascript_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,3 +1,16 @@
 // 0-javascript_is_amazing.js
 ["console.log()"]
-let myVar = ""
+// 0-javascript_is_amazing.js
+["console.log()"]
+val myVar = ""
+[Got]
+
+(0 chars long)
+[stderr]: 
+(0 chars long)
+[Expected]
+JavaScript is amazing
+
+(22 chars long)
+[stderr]: [Anything]
+(0 chars long)
